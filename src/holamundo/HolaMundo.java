85e6fc3ae05f20bd7df0014a80294f71ebe5bc7b
Estudiante <Estudiante@10.203.3.158>
2018-08-13 08:32:45 -0500

    Comit inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5;
+        
+        if(a==5){
+            System.out.println("izzi");
+        }
+        else
+        {
+            System.out.println("nel");
+        }
+        System.out.println("Q pex?");
+    
+    }
+    
+}
